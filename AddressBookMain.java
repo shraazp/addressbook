2aeec4dff589386828ae2730e6b2e4be84268e4b
shraazp <shravyap99@gmail.com>
2021-08-21 22:54:18 +0530

    added:UseCase 3 edit option is added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -49,6 +49,15 @@ public class AddressBookMain {
 		this.phone_number=phone_number;
 		this.email=email;
 	}
+	public static void delete(int c,AddressBookMain[] contact)
+	{
+		int i;
+		for(i=c;i<contact.length-1;i++)
+		{
+			contact[i]=contact[i+1];
+		}
+		contact[i]=null;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -92,36 +101,57 @@ public class AddressBookMain {
 				n++;
 				break;
 			
-			case 2:System.out.println("Enter the person whose name to be edited");
-				int c=sc.nextInt();
-				System.out.println("first Name:");
-				fname=sc.next();
-				System.out.println("last name:");
-				lname=sc.next();
-				System.out.println("address:");
-				address=sc.next();
-				System.out.println("city:");
-				city=sc.next();
-				System.out.println("state:");
-				state=sc.next();
-				System.out.println("zip:");
-				zip=sc.next();
-				System.out.println("phone number");
-				pno=sc.next();
-				System.out.println("E-mail:");
-				email=sc.next();
-				contact[c].edit(fname,lname,address,city,state,zip,pno,email);
-				break;
-			case 3:System.out.println("Enter the person whose name to be edited");
-				int c1=sc.nextInt();
-				contact[c1].display();
-				break;
-			case 4:counter=1;
-			break;
+			case 2:System.out.println("Enter the person whose contact to be edited");	
+				String name=sc.next();
+					for (int j=0;j<n;j++)
+					{
+						if(contact[j].first_name.equals(name))
+						{
+							int c1=j;
+							System.out.println("first Name:");
+							fname=sc.next();
+							System.out.println("last name:");
+							lname=sc.next();
+							System.out.println("address:");
+							address=sc.next();
+							System.out.println("city:");
+							city=sc.next();
+							System.out.println("state:");
+							state=sc.next();
+							System.out.println("zip:");
+							zip=sc.next();
+							System.out.println("phone number:");
+							pno=sc.next();
+							System.out.println("E-mail:");
+							email=sc.next();
+							contact[c1].edit(fname,lname,address,city,state,zip,pno,email);
+						}
+					}
+					break;
+				   
+			
+						
+			case 3: System.out.println("Enter the person whose contact to be displayed");
+					String name11=sc.next();
+					for (int j=0;j<n;j++)
+					{
+						if(contact[j].first_name.equals(name11))
+						{
+							int c1=j;
+							contact[c1].display();
+						}
+					   
+					}
+				
+					break;
+			case 4: counter=1;
+					break;
 			}
+			
 			
 		}
 		
 	}
+	
 
 }
