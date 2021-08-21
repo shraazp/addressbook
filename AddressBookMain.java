6de16f9eae814b3fe2f60263122676bf180e09f3
shraazp <shravyap99@gmail.com>
2021-08-21 16:12:41 +0530

    first commit

---
+++ AddressBookMain.java
@@ -0,0 +1,10 @@
+package address;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to adress book program");
+	}
+
+}
