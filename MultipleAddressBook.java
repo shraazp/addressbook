8109f86b84a5fe866c89d6f68a94d85909e46b6a
shraazp <shravyap99@gmail.com>
2021-08-25 11:16:39 +0530

    refactored:Multiple addressbook can be added and stored in dictionary

--- MultipleAddressBook.java
+++ MultipleAddressBook.java
@@ -35,28 +35,31 @@ public class MultipleAddressBook {  //different address book
 		
 		
 		
-		Hashtable<Integer,AddressBookMain>  multipleAddressBook = new Hashtable<Integer, AddressBookMain>();  //dictionary for mapping different address book with key 
+		Hashtable<String,AddressBookMain>  multipleAddressBook = new Hashtable<String, AddressBookMain>();  //dictionary for mapping different address book with key 
 		AddressBookMain object=new AddressBookMain();					//first addressbook
 		
-		int k=0,n;
+		String n="";
+		String k="AddressBook1";
 		multipleAddressBook.put(k, object);							//put the first addressbook into dictionary
-		k++;
+		
 		while(counter!=1) //it repeats till the user wants to end
 		{
 			System.out.println("Enter your choice\n1.add contact\n2.edit contact\n3.Delete contact\n4.Display contact\n5.Display addressbook\n6.Create another address book\n7.exit"); //options for different actions
 			int choice=sc.nextInt();
 			if(choice==6)  //to create a new address book
 			{
+				System.out.println("Enter the name of the addressbook");
+				n=sc.next();
 				AddressBookMain object1=new AddressBookMain();  // a new addressbook object is created 
-				multipleAddressBook.put(k, object1);
-				k++;	
+				multipleAddressBook.put(n, object1);
+					
 			}
 			else if(choice==7)				//to exist from the program
 				counter=1;
 			else
 			{
 				System.out.println("Enter the addressbook where you want to insert or modify the  contact!!");
-				n=sc.nextInt();
+				n=sc.next();
 				choice(choice,multipleAddressBook.get(n));   //to perform required operation on desired addressbook
 			}
 			
